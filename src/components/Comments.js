472a7865bf62190466937abf43c9eac5c6a9c7f6
emin <magomedemin.islamov@mail.ru>
2021-02-11 01:10:40 +0300

    added comments

---
+++ src/components/Comments.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+function Comments (props) {
+  const comment = useSelector(state => state.bigImageReducer.items.comments)
+  
+  return (
+    <div>
+      {comment === undefined ? '' : (
+        <div>
+          {comment[0] === undefined ? '' : (
+            <div>
+              <div className="data">
+                18.12.2019
+              </div>
+              <div>
+                {comment[0].text}
+              </div>
+            </div>
+          )}
+        </div>
+      )}
+    </div>
+  )
+}
+
+export default Comments
--- src/components/Modal.js
+++ src/components/Modal.js
@@ -2,7 +2,7 @@ import React, { useEffect } from 'react'
 import { useParams } from 'react-router-dom'
 import { useDispatch, useSelector } from 'react-redux'
 import { loadPic } from '../redux/actions'
-import { formatTime } from 'redux-logger/src/helpers'
+import Comments from './Comments'
 
 
 
@@ -30,14 +30,19 @@ function Modal ({active, setActive}) {
         onClick={e => e.stopPropagation()}>
         {loading ? <div>Идет загрузка</div> : (
           <div>
-            <div className="pict">
-              <img src={pic.url} alt=""/>
+            <div className="pic-and-comment">
+              <div className="pict">
+                <img src={pic.url} alt=""/>
+              </div>
+              <div className="comment">
+                <Comments />
+              </div>
             </div>
             <div className="pic-form">
               <form>
                 <input type="text" placeholder="Ваше имя" className="name"/>
                 <div>
-                  <input type="text" placeholder="Ваша комментарий" className="comment"/>
+                  <input type="text" placeholder="Ваша комментарий" className="your-comment"/>
                 </div>
               </form>
               <div>
